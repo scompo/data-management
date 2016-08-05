b872590cf278f040238bec3291a7a80b8777fd0a
Mauro Scomparin <scompo@gmail.com>
2016-08-05 17:57:02 +0200

    implemented TestAll

--- domain/project_test.go
+++ domain/project_test.go
@@ -1,11 +1,31 @@
 package domain
 
-import "testing"
+import (
+	"github.com/stretchr/testify/require"
+	"testing"
+	"time"
+)
 
 func TestAdd(t *testing.T) {
 	t.Fatal("to do")
 }
 
 func TestAll(t *testing.T) {
-	t.Fatal("to do")
+	require := require.New(t)
+	p := Project{
+		Name:         "test project",
+		Description:  "test project description",
+		CreationDate: time.Now(),
+		LastEdit:     time.Now(),
+	}
+	ps := []Project{p}
+	repo := ProjectsRepository{
+		projects: ps,
+	}
+	actual := repo.All()
+	require.Equal(1, len(actual), "not all data returned")
+	require.Equal(p.Name, actual[0].Name)
+	require.Equal(p.Description, actual[0].Description)
+	require.Equal(p.CreationDate, actual[0].CreationDate)
+	require.Equal(p.LastEdit, actual[0].LastEdit)
 }
