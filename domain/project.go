acd12122b0cb9ec1f61bdadbbad87348230f63ca
Mauro Scomparin <scompo@gmail.com>
2016-08-09 20:32:50 +0200

    added encode and test.

--- domain/project.go
+++ domain/project.go
@@ -1,10 +1,26 @@
 package domain
 
-import "time"
+import (
+	"encoding/json"
+	"io"
+	"time"
+)
+
+var projects map[string]Project = make(map[string]Project)
 
 type Project struct {
 	Name         string
 	Description  string
 	CreationDate time.Time
 	LastEdit     time.Time
+}
+
+var currentTime = time.Now
+
+func (p *Project) Encode(w io.Writer) {
+	p.CreationDate = currentTime()
+	p.LastEdit = currentTime()
+	enc := json.NewEncoder(w)
+	enc.Encode(p)
+	return
 }
--- domain/project_test.go
+++ domain/project_test.go
@@ -1,1 +1,45 @@
 package domain
+
+import (
+	"bytes"
+	"encoding/json"
+	"github.com/stretchr/testify/require"
+	"testing"
+	"time"
+)
+
+var testTime time.Time = time.Now()
+
+func TestSave(t *testing.T) {
+	require := require.New(t)
+	currentTime = func() time.Time {
+		return testTime
+	}
+	testData := []struct {
+		value    Project
+		expected Project
+	}{
+		{
+			Project{
+				Name:        "testName",
+				Description: "testDescription",
+			},
+			Project{
+				Name:         "testName",
+				Description:  "testDescription",
+				CreationDate: testTime,
+				LastEdit:     testTime,
+			},
+		},
+	}
+	for _, test := range testData {
+		var buf bytes.Buffer
+		test.value.Encode(&buf)
+		expData, err := json.Marshal(test.expected)
+		if err != nil {
+			t.Errorf("%v", err)
+		}
+		require.Equal(string(expData[:])+"\n", buf.String())
+	}
+	currentTime = time.Now
+}
