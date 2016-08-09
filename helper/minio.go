c61d8cb12a68377aefca18bc5187b6a27925ad17
Mauro Scomparin <scompo@gmail.com>
2016-08-09 20:56:15 +0200

    added minio helper

---
+++ helper/minio.go
@@ -0,0 +1,7 @@
+package helper
+
+import "github.com/scompo/data-management/domain"
+
+func NewProject(*domain.Project) domain.Project {
+	return domain.Project{}
+}
---
+++ helper/minio_test.go
@@ -0,0 +1,11 @@
+package helper
+
+import (
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+func TestSaveProjectWith(t *testing.T) {
+	require := require.New(t)
+	require.Fail("to do")
+}
