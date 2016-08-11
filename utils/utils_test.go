beb5b049bb736750aee5ba55f6db8dd67004d670
Mauro Scomparin <scompo@gmail.com>
2016-08-11 23:27:08 +0200

    added failFunction and test in utils_test.go

--- utils/utils_test.go
+++ utils/utils_test.go
@@ -1,7 +1,22 @@
 package utils
 
-import "testing"
+import (
+	"errors"
+	"net/http"
+	"testing"
+)
+
+func failFunction(w http.ResponseWriter, r *http.Request) error {
+	return errors.New("I always do fail!")
+}
+
+func TestFailFunction(t *testing.T) {
+	err := failFunction(nil, nil)
+	if err == nil {
+		t.Errorf("should return an error")
+	}
+}
 
 func TestServeHttp(t *testing.T) {
-	t.Errorf("to do\n")
+	t.Errorf("todo")
 }
