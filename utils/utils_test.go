c104233a8108db4248c7330c3e02689355b5743d
Mauro Scomparin <scompo@gmail.com>
2016-08-11 23:53:01 +0200

    implemented TestServeHTTP

--- utils/utils_test.go
+++ utils/utils_test.go
@@ -3,20 +3,29 @@ package utils
 import (
 	"errors"
 	"net/http"
+	"net/http/httptest"
+	"strings"
 	"testing"
 )
 
 func failFunction(w http.ResponseWriter, r *http.Request) error {
-	return errors.New("I always do fail!")
+	return errors.New("fail")
 }
 
 func TestFailFunction(t *testing.T) {
 	err := failFunction(nil, nil)
 	if err == nil {
-		t.Errorf("should return an error")
+		t.Errorf("should return an error\n")
 	}
 }
 
-func TestServeHttp(t *testing.T) {
-	t.Errorf("todo")
+func TestServeHTTP(t *testing.T) {
+	w := httptest.NewRecorder()
+	AppHandler(failFunction).ServeHTTP(w, nil)
+	if w.Code != http.StatusInternalServerError {
+		t.Errorf("should return %v but returned %v\n", http.StatusInternalServerError, w.Code)
+	}
+	if b := w.Body.String(); !strings.Contains(b, "fail") {
+		t.Errorf("returned something else in the body: \"%v\"\n", b)
+	}
 }
